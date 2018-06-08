ee71e34246e3a429785526816002c914cd191713
Simon NYSSEN <Simon.NYSSEN@outlook.be>
2018-06-08 13:32:57 +0200

    initial commit + interface + communication basique

---
+++ Joystick_Calibration_Tool/FormCalibration.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Joystick_Calibration_Tool
+{
+    partial class FormCalibration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCalibration));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CB_Port = new System.Windows.Forms.ComboBox();
+            this.CB_Baud = new System.Windows.Forms.ComboBox();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.SP_Joy = new System.IO.Ports.SerialPort(this.components);
+            this.RTB_Recu = new System.Windows.Forms.RichTextBox();
+            this.gb_Joystick = new System.Windows.Forms.GroupBox();
+            this.lblJoystick = new System.Windows.Forms.Label();
+            this.gb_Throttle = new System.Windows.Forms.GroupBox();
+            this.lblThrottle = new System.Windows.Forms.Label();
+            this.gb_Boutons = new System.Windows.Forms.GroupBox();
+            this.lblMissile = new System.Windows.Forms.Label();
+            this.lblGachette = new System.Windows.Forms.Label();
+            this.pbMissile = new System.Windows.Forms.PictureBox();
+            this.pbGachette = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cbMessages = new System.Windows.Forms.CheckBox();
+            this.gb_Joystick.SuspendLayout();
+            this.gb_Throttle.SuspendLayout();
+            this.gb_Boutons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMissile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGachette)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Bauds :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Port :";
+            // 
+            // CB_Port
+            // 
+            this.CB_Port.FormattingEnabled = true;
+            this.CB_Port.Location = new System.Drawing.Point(72, 7);
+            this.CB_Port.Margin = new System.Windows.Forms.Padding(2);
+            this.CB_Port.Name = "CB_Port";
+            this.CB_Port.Size = new System.Drawing.Size(92, 21);
+            this.CB_Port.TabIndex = 10;
+            this.CB_Port.SelectedIndexChanged += new System.EventHandler(this.CB_Port_SelectedIndexChanged);
+            this.CB_Port.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CB_Port_MouseClick);
+            // 
+            // CB_Baud
+            // 
+            this.CB_Baud.FormattingEnabled = true;
+            this.CB_Baud.Location = new System.Drawing.Point(72, 36);
+            this.CB_Baud.Margin = new System.Windows.Forms.Padding(2);
+            this.CB_Baud.Name = "CB_Baud";
+            this.CB_Baud.Size = new System.Drawing.Size(92, 21);
+            this.CB_Baud.TabIndex = 9;
+            this.CB_Baud.SelectedIndexChanged += new System.EventHandler(this.CB_Baud_SelectedIndexChanged);
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConnexion.Location = new System.Drawing.Point(13, 61);
+            this.btnConnexion.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(153, 23);
+            this.btnConnexion.TabIndex = 13;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = false;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // SP_Joy
+            // 
+            this.SP_Joy.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(this.SP_Joy_ErrorReceived);
+            this.SP_Joy.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SP_Joy_DataReceived);
+            // 
+            // RTB_Recu
+            // 
+            this.RTB_Recu.BackColor = System.Drawing.SystemColors.WindowText;
+            this.RTB_Recu.DetectUrls = false;
+            this.RTB_Recu.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.RTB_Recu.Location = new System.Drawing.Point(12, 106);
+            this.RTB_Recu.Margin = new System.Windows.Forms.Padding(6);
+            this.RTB_Recu.Name = "RTB_Recu";
+            this.RTB_Recu.ReadOnly = true;
+            this.RTB_Recu.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.RTB_Recu.Size = new System.Drawing.Size(153, 256);
+            this.RTB_Recu.TabIndex = 14;
+            this.RTB_Recu.Text = "";
+            this.RTB_Recu.WordWrap = false;
+            // 
+            // gb_Joystick
+            // 
+            this.gb_Joystick.Controls.Add(this.lblJoystick);
+            this.gb_Joystick.Location = new System.Drawing.Point(247, 7);
+            this.gb_Joystick.Name = "gb_Joystick";
+            this.gb_Joystick.Size = new System.Drawing.Size(250, 250);
+            this.gb_Joystick.TabIndex = 16;
+            this.gb_Joystick.TabStop = false;
+            this.gb_Joystick.Text = "Joystick";
+            // 
+            // lblJoystick
+            // 
+            this.lblJoystick.AutoSize = true;
+            this.lblJoystick.Location = new System.Drawing.Point(0, 237);
+            this.lblJoystick.Name = "lblJoystick";
+            this.lblJoystick.Size = new System.Drawing.Size(35, 13);
+            this.lblJoystick.TabIndex = 1;
+            this.lblJoystick.Text = "label3";
+            // 
+            // gb_Throttle
+            // 
+            this.gb_Throttle.Controls.Add(this.lblThrottle);
+            this.gb_Throttle.Location = new System.Drawing.Point(171, 7);
+            this.gb_Throttle.Name = "gb_Throttle";
+            this.gb_Throttle.Size = new System.Drawing.Size(70, 355);
+            this.gb_Throttle.TabIndex = 17;
+            this.gb_Throttle.TabStop = false;
+            this.gb_Throttle.Text = "Throttle";
+            // 
+            // lblThrottle
+            // 
+            this.lblThrottle.AutoSize = true;
+            this.lblThrottle.Location = new System.Drawing.Point(-3, 342);
+            this.lblThrottle.Name = "lblThrottle";
+            this.lblThrottle.Size = new System.Drawing.Size(35, 13);
+            this.lblThrottle.TabIndex = 0;
+            this.lblThrottle.Text = "label3";
+            // 
+            // gb_Boutons
+            // 
+            this.gb_Boutons.Controls.Add(this.lblMissile);
+            this.gb_Boutons.Controls.Add(this.lblGachette);
+            this.gb_Boutons.Controls.Add(this.pbMissile);
+            this.gb_Boutons.Controls.Add(this.pbGachette);
+            this.gb_Boutons.Location = new System.Drawing.Point(247, 263);
+            this.gb_Boutons.Name = "gb_Boutons";
+            this.gb_Boutons.Size = new System.Drawing.Size(250, 99);
+            this.gb_Boutons.TabIndex = 18;
+            this.gb_Boutons.TabStop = false;
+            this.gb_Boutons.Text = "Boutons";
+            // 
+            // lblMissile
+            // 
+            this.lblMissile.AutoSize = true;
+            this.lblMissile.Location = new System.Drawing.Point(171, 27);
+            this.lblMissile.Name = "lblMissile";
+            this.lblMissile.Size = new System.Drawing.Size(38, 13);
+            this.lblMissile.TabIndex = 3;
+            this.lblMissile.Text = "Missile";
+            // 
+            // lblGachette
+            // 
+            this.lblGachette.AutoSize = true;
+            this.lblGachette.Location = new System.Drawing.Point(44, 27);
+            this.lblGachette.Name = "lblGachette";
+            this.lblGachette.Size = new System.Drawing.Size(51, 13);
+            this.lblGachette.TabIndex = 2;
+            this.lblGachette.Text = "Gachette";
+            // 
+            // pbMissile
+            // 
+            this.pbMissile.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbMissile.Image = global::Joystick_Calibration_Tool.Properties.Resources._2000px_Redbutton_svg;
+            this.pbMissile.Location = new System.Drawing.Point(159, 43);
+            this.pbMissile.Name = "pbMissile";
+            this.pbMissile.Size = new System.Drawing.Size(50, 50);
+            this.pbMissile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMissile.TabIndex = 1;
+            this.pbMissile.TabStop = false;
+            // 
+            // pbGachette
+            // 
+            this.pbGachette.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbGachette.Image = global::Joystick_Calibration_Tool.Properties.Resources._2000px_Redbutton_svg;
+            this.pbGachette.Location = new System.Drawing.Point(47, 43);
+            this.pbGachette.Name = "pbGachette";
+            this.pbGachette.Size = new System.Drawing.Size(50, 50);
+            this.pbGachette.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGachette.TabIndex = 0;
+            this.pbGachette.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cbMessages
+            // 
+            this.cbMessages.AutoSize = true;
+            this.cbMessages.Checked = true;
+            this.cbMessages.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbMessages.Location = new System.Drawing.Point(14, 89);
+            this.cbMessages.Name = "cbMessages";
+            this.cbMessages.Size = new System.Drawing.Size(144, 17);
+            this.cbMessages.TabIndex = 19;
+            this.cbMessages.Text = "Derniers messages reçus";
+            this.cbMessages.UseVisualStyleBackColor = true;
+            this.cbMessages.CheckedChanged += new System.EventHandler(this.cbMessages_CheckedChanged);
+            // 
+            // FormCalibration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 374);
+            this.Controls.Add(this.cbMessages);
+            this.Controls.Add(this.gb_Boutons);
+            this.Controls.Add(this.gb_Throttle);
+            this.Controls.Add(this.gb_Joystick);
+            this.Controls.Add(this.RTB_Recu);
+            this.Controls.Add(this.btnConnexion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CB_Port);
+            this.Controls.Add(this.CB_Baud);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormCalibration";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calibration du joystick";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormCalibration_FormClosing);
+            this.gb_Joystick.ResumeLayout(false);
+            this.gb_Joystick.PerformLayout();
+            this.gb_Throttle.ResumeLayout(false);
+            this.gb_Throttle.PerformLayout();
+            this.gb_Boutons.ResumeLayout(false);
+            this.gb_Boutons.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMissile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGachette)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CB_Port;
+        private System.Windows.Forms.ComboBox CB_Baud;
+        private System.Windows.Forms.Button btnConnexion;
+        private System.IO.Ports.SerialPort SP_Joy;
+        private System.Windows.Forms.RichTextBox RTB_Recu;
+        private System.Windows.Forms.GroupBox gb_Joystick;
+        private System.Windows.Forms.GroupBox gb_Throttle;
+        private System.Windows.Forms.GroupBox gb_Boutons;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox cbMessages;
+        private System.Windows.Forms.Label lblJoystick;
+        private System.Windows.Forms.Label lblThrottle;
+        private System.Windows.Forms.PictureBox pbGachette;
+        private System.Windows.Forms.PictureBox pbMissile;
+        private System.Windows.Forms.Label lblMissile;
+        private System.Windows.Forms.Label lblGachette;
+    }
+}
+
---
+++ Joystick_Calibration_Tool/FormCalibration.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Joystick_Calibration_Tool
+{
+    public partial class FormCalibration : Form
+    {
+        int[] Liste_Bauds = new int[]
+        {
+            50,
+            75,
+            110,
+            150,
+            300,
+            600,
+            1200,
+            1800,
+            2400,
+            4800,
+            7200,
+            9600,
+            14400,
+            19200,
+            38400,
+            56000,
+            57600,
+            76800,
+            115200,
+            128000,
+            230400,
+            256000,
+            460800
+        };
+        int NbrLignes = 0;
+        DonneesJoy donneesJoy = new DonneesJoy();
+
+        public FormCalibration()
+        {
+            InitializeComponent();
+            foreach (int Baud in Liste_Bauds) //Affiche les valeurs dans la CB
+            {
+                CB_Baud.Items.Add(Baud);
+            }
+            RTB_Recu.Text = "Appuyez sur \"Connexion\" après avoir entré le port et le baudrate à utiliser pour continuer...";
+            CB_Baud.SelectedIndex = 11; // 9600 bauds de sélectionné par défaut
+        }
+
+        private void SP_Joy_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            try
+            {
+                string Donnees = sp.ReadLine();
+                string[] SplitDonnees = Donnees.Split(';');
+                if (SplitDonnees.Length >= 5) // Il peut arriver, notamment lorsqu'on vient d'ouvrir la connexion, qu'on ne reçoive pas les messages en entier; il est donc important de faire toutes les vérifs suivantes au cas où le message serait corrompu
+                {
+                    // Rappel : TryParse renvoie vrai si la conversion réussi, sinon faux
+                    bool IsvGachette = int.TryParse(SplitDonnees[0], out int vGachette);
+                    bool IsvMissile = int.TryParse(SplitDonnees[1], out int vMissile);
+                    bool IsvAxeX = int.TryParse(SplitDonnees[2], out int vAxeX);
+                    bool IsvAxeY = int.TryParse(SplitDonnees[3], out int vAxeY);
+                    bool IsvThrottle = int.TryParse(SplitDonnees[4], out int vThrottle);
+                    // Si il y a erreur de conversion, on envoie la valeur par défaut => pour un bouton, bouton non appuyé | pour un axe, axe au centre
+                    donneesJoy = new DonneesJoy(IsvGachette ? vGachette : 0, IsvMissile ? vMissile : 0, IsvAxeX ? vAxeX : 128, IsvAxeY ? vAxeY : 128, IsvThrottle ? vThrottle : 128);
+                }
+            }
+            catch (Exception ex)
+            {
+                //RTB_Recu.Text = "Une exception sur le port série a été détectée !\n" + ex.ToString();
+                MessageBox.Show("Une exception sur le port série a été détectée !\n" + ex.ToString(), "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SP_Joy_ErrorReceived(object sender, System.IO.Ports.SerialErrorReceivedEventArgs e)
+        {
+            Close_Serial_Port(SP_Joy);
+            MessageBox.Show("Erreur sur le port série !\n\n" + e.ToString(), "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void CB_Port_MouseClick(object sender, MouseEventArgs e)
+        {
+            Close_Serial_Port(SP_Joy);
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames(); //Récupère les ports utilisés
+            CB_Port.Items.Clear();
+
+            // Affiche les noms des ports utilisés dans la CB
+            foreach (string port in ports)
+            {
+                CB_Port.Items.Add(port);
+            }
+        }
+
+        private void CB_Port_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Close_Serial_Port(SP_Joy);
+            SP_Joy.PortName = (string)CB_Port.SelectedItem;
+        }
+
+        private void Close_Serial_Port(SerialPort sp)
+        {
+            sp.Close();
+            //RTB_Recu.Text = "Connexion interrompue";
+            btnConnexion.Enabled = true;
+            timer1.Stop();
+            timer1.Enabled = false;
+        }
+
+        private void CB_Baud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Close_Serial_Port(SP_Joy);
+            SP_Joy.BaudRate = (int)CB_Baud.SelectedItem;
+        }
+
+        private void btnConnexion_Click(object sender, EventArgs e)
+        {
+            if (CB_Port.SelectedItem == null)
+                MessageBox.Show("Veuillez choisir un port avant de continuer !"); // Le port défini par défaut (COM1) n'existe pas forcément sur l'ordinateur de l'utilisateur, et pourrait donc causer un crash dans ce cas. On force donc l'utilisateur à choisir un port valable
+            else
+            {
+                SP_Joy.Open();
+                if (SP_Joy.IsOpen)
+                {
+                    RTB_Recu.Text = "Connexion établie";
+                    btnConnexion.Enabled = false;
+                    timer1.Enabled = true;
+                    timer1.Start();
+                }
+                else
+                    MessageBox.Show("Erreur lors de l'ouverture de la connexion !");
+            }
+        }
+
+        private void FormCalibration_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Close_Serial_Port(SP_Joy);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // tick toutes les 10ms
+        {
+            if (cbMessages.Checked)
+            {
+                if (NbrLignes % 10 == 0) // On ne met à jour la console que toutes les 100 ms pour éviter de la surcharger
+                {
+                    if (NbrLignes == 0 || NbrLignes > 190) // Valeur = max nbr lignes que peut supporter notre RTB sans scroll * 10 vu qu'on utilise également NbrLignes comme compteur
+                    {
+                        RTB_Recu.Text = "";
+                        NbrLignes = 0;
+                    }
+                    RTB_Recu.Text += donneesJoy.vGachette.ToString() + "/" + donneesJoy.vMissile.ToString() + "/" + donneesJoy.vAxeX + "/" + donneesJoy.vAxeY + "/" + donneesJoy.vThrottle + "\n";
+                }
+                NbrLignes++;
+            }
+            DrawAll();
+        }
+
+        private void cbMessages_CheckedChanged(object sender, EventArgs e)
+        {
+            RTB_Recu.Text = "";
+            NbrLignes = 0;
+        }
+
+        private void DrawAll()
+        {
+            DrawButtons();
+            DrawThrottle();
+            DrawJoystick();
+        }
+        private void DrawThrottle()
+        {
+
+        }
+        private void DrawJoystick()
+        {
+
+        }
+        private void DrawButtons()
+        {
+            pbGachette.Image = donneesJoy.vGachette ? Properties.Resources._2000px_Button_Icon_Green_svg : Properties.Resources._2000px_Redbutton_svg;
+            pbMissile.Image =  donneesJoy.vMissile ? Properties.Resources._2000px_Button_Icon_Green_svg : Properties.Resources._2000px_Redbutton_svg;
+        }
+    }
+
+    class DonneesJoy
+    {
+        #region Donnees membres
+        private bool _vGachette;
+        private bool _vMissile;
+        private int _vAxeX;
+        private int _vAxeY;
+        private int _vThrottle;
+        #endregion
+        #region Constructeurs
+        public DonneesJoy() { }
+        public DonneesJoy(bool vGachette_, bool vMissile_, int vAxeX_, int vAxeY_, int vThrottle_)
+        {
+            vGachette = vGachette_;
+            vMissile = vMissile_;
+            vAxeX = vAxeX_;
+            vAxeY = vAxeY_;
+            vThrottle = vThrottle_;
+        }
+        public DonneesJoy(int vGachette_, int vMissile_, int vAxeX_, int vAxeY_, int vThrottle_)
+        {
+            if (vGachette_ > 0)
+                vGachette = true;
+            else
+                vGachette = false;
+            if (vMissile_ > 0)
+                vMissile = true;
+            else
+                vMissile = false;
+            vAxeX = vAxeX_;
+            vAxeY = vAxeY_;
+            vThrottle = vThrottle_;
+        }
+        #endregion
+        #region Event
+        // L'idée est ici de trigger un event lorsqu'un des valeurs de notre classe a changé (voir accesseurs pour l'appel)
+        public event System.EventHandler ValueChanged;
+        protected virtual void OnValueChanged()
+        {
+            if (ValueChanged != null)
+                ValueChanged(this, EventArgs.Empty);
+        }
+        #endregion
+        #region Accesseurs
+        public bool vGachette
+        {
+            get { return _vGachette; }
+            set { _vGachette = value; OnValueChanged(); }
+        }
+        public bool vMissile
+        {
+            get { return _vMissile; }
+            set { _vMissile = value; OnValueChanged(); }
+        }
+        public int vAxeX
+        {
+            get { return _vAxeX; }
+            set { _vAxeX = value; OnValueChanged(); }
+        }
+        public int vAxeY
+        {
+            get { return _vAxeY; }
+            set { _vAxeY = value; OnValueChanged(); }
+        }
+        public int vThrottle
+        {
+            get { return _vThrottle; }
+            set { _vThrottle = value; OnValueChanged(); }
+        }
+        #endregion
+    }
+}
